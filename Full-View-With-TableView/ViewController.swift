4cc176bf4113b9615699db43d20a165b15b15aa9
Muhammad Rajab <ichijo_rajab@hotmail.com>
2024-07-08 15:23:28 +0700

    Initial Commit

---
+++ Full-View-With-TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Full-View-With-TableView
+//
+//  Created by Muhammad Rajab on 08-07-2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
